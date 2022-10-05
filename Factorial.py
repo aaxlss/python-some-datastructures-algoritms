4c76da7bc3de378060d76b098e74c97f666d7a1c
aaxlss <axl9305@gmail.com>
2022-10-05 15:46:20 +0100

    [Update] Adding new method for fibonacci recursion

--- Factorial.py
+++ Factorial.py
@@ -4,5 +4,14 @@ def factorialRecursive(number):
     else:
         return number * factorialRecursive(number - 1)
 
+def fibonacciRecursive(iteration):
+
+    if iteration < 2:
+        return iteration
+    else:
+        return fibonacciRecursive(iteration - 1) + fibonacciRecursive(iteration - 2)
+
+
 if __name__ == '__main__':
-    print(factorialRecursive(5))
+    # print(factorialRecursive(5))
+    print(fibonacciRecursive(43))
